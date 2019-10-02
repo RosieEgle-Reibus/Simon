d749ef08530c4d4e3516addd77e8f529d323bfc6
Rose Egle <rosiemegle89@gmail.com>
2019-10-02 19:32:17 -0400

    made a play sound function and included it in event listener

--- script.js
+++ script.js
@@ -5,10 +5,24 @@ let swipeButton = document.getElementById('swipe-button')
 let dragButton = document.getElementById('drag-button')
 let spaceButton = document.getElementById('sapce-button')
 
+// function sound(src) {
+//     startButton.sound = document.createElement("audio");
+//     startButton.sound.src = src;
+//     startButton.sound.setAttribute("preload", "auto");
+//     startButton.sound.setAttribute("controls", "none");
+//     startButton.sound.style.display = "none";
+//     startButton.appendChild(this.sound);
+//     startButton.play = function(){
+//         startButton.sound.play();
+//     }
 
-
+let startSound = document.getElementById("tryit")
 
 let startButton = document.getElementById("start-button")
+
+playAudio = () => {
+    startSound.play() 
+}
 
 changeColorOn = (buttonName) => {
     buttonName.classList.add("on")
@@ -21,9 +35,6 @@ changeColorOff = (buttonName) => {
 startButton.addEventListener("click", function(event){
     event.preventDefault()
     changeColorOn(startButton) 
-    setTimeout(changeColorOff, 1000, startButton)  
+    setTimeout(changeColorOff, 1000, startButton) 
+    playAudio()
 })
-
-
-
-
