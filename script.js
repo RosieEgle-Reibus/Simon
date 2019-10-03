6daca7d0008382062a8d369ba5b1aab6edf42349
Rose Egle <rosiemegle89@gmail.com>
2019-10-03 09:38:03 -0400

    made randomizing function

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let swipeButton = document.getElementById('swipe-button')
 let dragButton = document.getElementById('drag-button')
 let spaceButton = document.getElementById('space-button')
 let startButton = document.getElementById("start-button")
+let randomButton
 
 //Sound Variables
 let clickSound = document.getElementById("clickSound")
@@ -13,9 +14,16 @@ let dragSound = document.getElementById("dragSound")
 let swipeSound = document.getElementById("swipeSound")
 let spaceSound = document.getElementById("spaceSound")
 
+//Arrays
+let buttons = [clickButton, swipeButton, dragButton, spaceButton]
+let userArray = []
+let startArray = [] 
 
 
-
+randomizer = () => {
+randomButton = buttons[Math.floor(Math.random()*buttons.length)]
+startArray.push(randomButton)
+}
 
 // Global Functions
 playAudio = (button) => {
@@ -27,7 +35,6 @@ changeColorOn = (buttonName) => {
 changeColorOff = (buttonName) => {
     buttonName.classList.remove("on")
 }
-
 lightAndSound = (buttonName, sound) => {
     changeColorOn(buttonName) 
     setTimeout(changeColorOff, 1000, buttonName) 
@@ -35,12 +42,15 @@ lightAndSound = (buttonName, sound) => {
 }
 
 // Event Listeners
+
+//Start Button
 startButton.addEventListener("click", function(event){
     event.preventDefault()
     changeColorOn(startButton) 
-    setTimeout(changeColorOff, 1000, startButton)
-    
+    setTimeout(changeColorOff, 1000, startButton)   
 })
+
+//Buttons
 clickButton.addEventListener("click", function(event){
     event.preventDefault()
     lightAndSound(clickButton, clickSound)
@@ -57,3 +67,5 @@ spaceButton.addEventListener("click", function(event){
     event.preventDefault()
     lightAndSound(spaceButton, spaceSound)
 })
+
+
