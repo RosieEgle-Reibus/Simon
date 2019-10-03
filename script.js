80089bb1a5f3764c04dd5609f765854b2432f2cf
Rose Egle <rosiemegle89@gmail.com>
2019-10-02 20:07:22 -0400

    added click event listener for each button and made universal light and sound function

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 // Global Variables
 
+// Buttons
 let clickButton = document.getElementById('click-button')
 let swipeButton = document.getElementById('swipe-button')
 let dragButton = document.getElementById('drag-button')
 let spaceButton = document.getElementById('sapce-button')
-
-// function sound(src) {
-//     startButton.sound = document.createElement("audio");
-//     startButton.sound.src = src;
-//     startButton.sound.setAttribute("preload", "auto");
-//     startButton.sound.setAttribute("controls", "none");
-//     startButton.sound.style.display = "none";
-//     startButton.appendChild(this.sound);
-//     startButton.play = function(){
-//         startButton.sound.play();
-//     }
-
-let startSound = document.getElementById("tryit")
-
 let startButton = document.getElementById("start-button")
 
-playAudio = () => {
-    startSound.play() 
-}
+//Sounds
+let clickSound = document.getElementById("clickSound")
+let dragSound = document.getElementById("dragSound")
+let swipeSound = document.getElementById("swipeSound")
+let spaceSound = document.getElementById("spaceSound")
 
+// Global Functions
+
+playAudio = (button) => {
+    button.play() 
+}
 changeColorOn = (buttonName) => {
     buttonName.classList.add("on")
 }
@@ -31,10 +25,30 @@ changeColorOff = (buttonName) => {
     buttonName.classList.remove("on")
 }
 
+lightAndSound = (buttonName, sound) => {
+    changeColorOn(buttonName) 
+    setTimeout(changeColorOff, 1000, buttonName) 
+    playAudio(sound)
+}
+
+
+
+// Event Listeners
 
 startButton.addEventListener("click", function(event){
     event.preventDefault()
-    changeColorOn(startButton) 
-    setTimeout(changeColorOff, 1000, startButton) 
-    playAudio()
+    lightAndSound(startButton)
+})
+
+clickButton.addEventListener("click", function(event){
+    event.preventDefault()
+    lightAndSound(clickButton, clickSound)
+})
+dragButton.addEventListener("click", function(event){
+    event.preventDefault()
+    lightAndSound(clickButton, startSound)
+})
+swipeButton.addEventListener("click", function(event){
+    event.preventDefault()
+    lightAndSound(clickButton, startSound)
 })
